34d21607ac1b994fdaf73198176aa46470b3b1ef
MaximTestov3108 <max1testov@gmail.com>
2020-02-02 16:58:20 +0300

    New

--- matrica/Form1.cs
+++ matrica/Form1.cs
@@ -12,9 +12,59 @@ namespace matrica
 {
     public partial class Form1 : Form
     {
+        TextBox[,] matrix_vis1;
+        TextBox[,] matrix_vis2;
+        TextBox[,] matrix_vis3;
+
+        int default_size = 5;
+
+
         public Form1()
         {
+
             InitializeComponent();
+            matrix_vis1 = new TextBox[,] {
+            {m1_00, m1_01, m1_02, m1_03, m1_04 },
+            {m1_10, m1_11, m1_12, m1_13, m1_14 },
+            {m1_20, m1_21, m1_22, m1_23, m1_24 },
+            {m1_30, m1_31, m1_32, m1_33, m1_34 },
+            {m1_40, m1_41, m1_42, m1_43, m1_44 }
+            };
+
+            matrix_vis2 = new TextBox[,] {
+            {m2_00, m2_01, m2_02, m2_03, m2_04},
+            {m2_10, m2_11, m2_12, m2_13, m2_14},
+            {m2_20, m2_21, m2_22, m2_23, m2_24},
+            {m2_30, m2_31, m2_32, m2_33, m2_34},
+            {m2_40, m2_41, m2_42, m2_43, m2_44}
+            };
+
+            matrix_vis3 = new TextBox[,] {
+            {m3_00, m3_01, m3_02, m3_03, m3_04},
+            {m3_10, m3_11, m3_12, m3_13, m3_14},
+            {m3_20, m3_21, m3_22, m3_23, m3_24},
+            {m3_30, m3_31, m3_32, m3_33, m3_34},
+            {m3_40, m3_41, m3_42, m3_43, m3_44},
+            };
+
+            int[,] numbers2 = new int[default_size, default_size];
+        }
+        private void show_numbers(TextBox[,]matrix, int[,] numbers)
+        {
+            for(int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for(int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    matrix[i, j].Text = numbers[i, j].ToString();
+                }
+            }
+        }
+
+        private void init_form()
+        {
+
         }
     }
 }
+
+    
--- matrica/Matrix.cs
+++ matrica/Matrix.cs
@@ -8,5 +8,34 @@ namespace matrica
 {
     class Matrix
     {
+        int[,] numbers;
+        
+        public Matrix(int i, int j)
+        {
+            numbers = new int[i, j];
+        }
+
+        public Matrix()
+        {
+            numbers = new int[5, 5];
+        }
+
+        public Matrix(int i, int j, int min, int max)
+        {
+
+            numbers = new int[i, j];
+            Random random = new Random();
+           for(int g = 0; g < numbers.GetLength(0); g++)
+            {
+                for(int b = 0; b < numbers.GetLength(1); b++)
+                {
+                    numbers[g, b] = random.Next(min, max + 1);
+                }
+            }
+        }
+        public Matrix(int[,] _numbers)
+        {
+            numbers = _numbers;
+        }
     }
 }
